b9cec40fcf12f681fe74310f41a9b64c1cef7363
Pranav Pradeep <pranavpradeep786@gmail.com>
2024-02-15 14:40:24 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./App.css"
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+  const [error, setError] = useState('');
+
+  const API_KEY = 'f00c38e0279b7bc85480c3fe775d518c'; // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+      setWeatherData(response.data);
+      setError('');
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setError('Error fetching weather data. Please try again.');
+    }
+  };
+
+
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchWeatherData();
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter city" value={city} onChange={handleChange} />
+        <button type="submit">Search City</button>
+      </form>
+      {error && <p>{error}</p>}
+      {weatherData && (
+        <div>
+          <h2>Weather in {weatherData.name}</h2>
+          <p>Description: {weatherData.weather[0].description}</p>
+          <p>Temperature: {weatherData.main.temp} Kelvin</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
